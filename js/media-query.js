e2262bcba6d8aae94bba2c3095a2bc9f9597b23d
yairmurcia <murciapradillarubenyair@gmail.com>
2023-05-15 08:45:36 -0500

    desplazamiento

--- js/media-query.js
+++ js/media-query.js
@@ -1,7 +1,21 @@
 let menuBar = document.getElementById("menuBar");
+let mainNav = document.getElementById("mainNav");
+let mainMenu = document.getElementById("mainMenu");
 
 menuBar.addEventListener("click", function() {
-    alert("Si acabas de llegar Bienvenido, si te vas vuelve pronto");
-
+    if(mainMenu.classList.contains("main-menu")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
+    }
+    else {
+        mainMenu.classList.add("main-menu");
+        mainMenu.classList.remove("main-menu-block");
+    }
 });
 
+/*     mainMenu.classList.remove("main-menu");
+    mainMenu.classList.add("main-menu-block"); */
+
+/*alert("Si acabas de llegar Bienvenido, si te vas vuelve pronto");*/
+    /* mainNav.classList.remove("main-nav");
+    mainNav.classList.add("main-nav-2"); */
