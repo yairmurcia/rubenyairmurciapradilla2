e0c4b4a411a2e4014869c03ce4de0071593e96ce
yairmurcia <murciapradillarubenyair@gmail.com>
2023-10-06 11:42:35 -0500

    modifi

--- js/sneakers.js
+++ js/sneakers.js
@@ -1,184 +1,45 @@
-// Cambio de cantidad de articulos ingresado por el usuario.
+let mainImg = document.getElementById("mainImg");
+let iconLeft = document.getElementById("iconLeft")
+let iconRight = document.getElementById("iconRight")
+let mainImgFixed1 = document.getElementById("mainImgFixed1")
+let mainImgFixed2 = document.getElementById("mainImgFixed2")
+let mainImgFixed3 = document.getElementById("mainImgFixed3")
+let mainImgFixed4 = document.getElementById("mainImgFixed4")
 
-const minusBtn = document.querySelector('.input__minus');
-const plusBtn = document.querySelector('.input__plus');
-let userInput = document.querySelector('.input__number');
-
-let userInputNumber = 0;
-
-plusBtn.addEventListener('click', ()=>{
-    userInputNumber++;
-    userInput.value = userInputNumber;
-    console.log(userInputNumber);
-});
-
-minusBtn.addEventListener('click', ()=>{
-    userInputNumber--;
-    if(userInputNumber <= 0){
-        userInputNumber = 0;
+iconRight.addEventListener("click", function() {
+    if (!mainImgFixed1.classList.contains("img-hidden")) {
+        mainImgFixed1.classList.add("img-hidden");
+        mainImgFixed2.classList.remove("img-hidden");
     }
-    userInput.value = userInputNumber;
-    console.log(userInputNumber);
-});
-
-// Agregar el total de productos al carrito cuando se presiona el boton ADD TO CART
-const addToCartBtn = document.querySelector('.details__button');
-let cartNotification = document.querySelector('.header__cart--notification');
-let lastValue = parseInt(cartNotification.innerText);
-
-addToCartBtn.addEventListener('click', ()=>{ 
-    lastValue = lastValue + userInputNumber;
-    
-    cartNotification.innerText = lastValue;
-    cartNotification.style.display = 'block';
-    drawProductInModal();
-    
-});
-
-//Mostrar el modal con el detalle del carrito
-const cartIconBtn = document.querySelector('.header__cart');
-const cartModal = document.querySelector('.cart-modal');
-// let priceModal = document.querySelector('.cart-modal__price');
-const productContainer = document.querySelector('.cart-modal__chekout-container');
-
-cartIconBtn.addEventListener('click', ()=>{
-    cartModal.classList.toggle('show');
-
-    if(lastValue === 0){
-        productContainer.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
-    }else{
-        drawProductInModal();
+    else if (!mainImgFixed2.classList.contains("img-hidden")) {
+        mainImgFixed2.classList.add("img-hidden");
+        mainImgFixed3.classList.remove("img-hidden");
     }
-    
-});
-
-//Borrar el contenido del carrito
-function deleteProduct(){
-    const deleteProductBtn = document.querySelector('.cart-modal__delete');
-    deleteProductBtn.addEventListener('click', ()=>{
-        productContainer.innerHTML = '<p class="cart-empty">Your cart is empty</p>';
-        lastValue = 0;
-        cartNotification.innerText = lastValue;
-    });
-}
-
-// Cambiar imagenes cuando se presione los botones flecha.
-const imageContainer = document.querySelector('.gallery__image-container');
-const previusGalleryBtn = document.querySelector('.gallery__previus');
-const nextGalleryBtn = document.querySelector('.gallery__next');
-let imgIndex = 1;
-
-nextGalleryBtn.addEventListener('click', ()=>{
-    changeNextImage(imageContainer);
-});
-
-previusGalleryBtn.addEventListener('click', ()=>{
-    changePreviusImage(imageContainer);
-});
-
-
-//Mostrar el modal de imagenes cuando hago click en la imagen principal.
-const imagesModal = document.querySelector('.modal-gallery__background');
-const closeModalBtn = document.querySelector('.modal-gallery__close');
-
-imageContainer.addEventListener('click', ()=>{
-    if(window.innerWidth >= 1115){
-        imagesModal.style.display = 'grid';
+    else if (!mainImgFixed3.classList.contains("img-hidden")) {
+        mainImgFixed3.classList.add("img-hidden");
+        mainImgFixed4.classList.remove("img-hidden");
     }
-    
-});
-
-closeModalBtn.addEventListener('click', ()=>{
-    imagesModal.style.display = 'none';
-});
-
-//Cambiar las imagenes principales desde los thumbnails
-let thumbnails = document.querySelectorAll('.gallery__thumnail')
-thumbnails = [...thumbnails]
-
-thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', event=>{
-        console.log(event.target.id)
-        imageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id}.jpg')`
-    });
-});
-
-//Cambiar las imagenes principales desde los thumbnails en el MODAL
-let modalthumbnails = document.querySelectorAll('.modal-gallery__thumnail');
-const modalImageContainer = document.querySelector('.modal-gallery__image-container')
-modalthumbnails = [...modalthumbnails]
-
-modalthumbnails.forEach(modalthumbnail => {
-    modalthumbnail.addEventListener('click', event=>{
-        console.log(event.target.id.slice(-1))
-        modalImageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id.slice(-1)}.jpg')`
-    });
-});
-
-// Cambiar imagen principal de modal desde flechas en el modal
-const previusModalBtn = document.querySelector('.modal-gallery__previus');
-const nextModalBtn = document.querySelector('.modal-gallery__next');
-
-nextModalBtn.addEventListener('click', ()=>{
-    changeNextImage(modalImageContainer);
-});
-
-previusModalBtn.addEventListener('click', ()=>{
-    changePreviusImage(modalImageContainer);
-});
-
-// Mostrar el navbar cuando presiono el menu de hamburgesa
-const hamburgerMenu = document.querySelector('.header__menu');
-const modalNavbar = document.querySelector('.modal-navbar__background');
-const closeModalNavbar = document.querySelector('.modal-navbar__close-icon');
-
-modalNavbar.style.display = 'none'
-
-hamburgerMenu.addEventListener('click', ()=>{
-    console.log('abrir modal');
-    modalNavbar.style.display = 'block';
-});
-
-closeModalNavbar.addEventListener('click', ()=>{
-    modalNavbar.style.display = 'none';
-});
-
-
-
-
-
-// FUNCIONES
-
-function drawProductInModal(){
-    productContainer.innerHTML = `
-        <div class="cart-modal__details-container">
-            <img class="cart-modal__image" src="./images/image-product-1-thumbnail.jpg" alt="">
-            <div>
-            <p class="cart-modal__product">Autumn Limited Edition...</p>
-            <p class="cart-modal__price">$125 x3 <span>$375.00</span> </p>
-            </div>
-            <img class="cart-modal__delete" src="./images/icon-delete.svg" alt="delete">
-        </div>
-        <button class="cart-modal__chekount" >Checkout</button>`
-    deleteProduct()
-    let priceModal = document.querySelector('.cart-modal__price');
-    priceModal.innerHTML = `$125 x${lastValue} <span>$${lastValue*125}.00</span>`;
-}
-
-function changeNextImage(imgContainer){
-    if(imgIndex === 4){
-        imgIndex = 1;
-    }else{
-        imgIndex++;
+    else if (!mainImgFixed4.classList.contains("img-hidden")) {
+        mainImgFixed4.classList.add("img-hidden");
+        mainImgFixed1.classList.remove("img-hidden");
     }
-    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`
-}
+});
 
-function changePreviusImage(imgContainer){
-    if(imgIndex === 1){
-        imgIndex = 4;
-    }else{
-        imgIndex--;
+iconLeft.addEventListener("click", function() {
+    if (!mainImgFixed4.classList.contains("img-hidden")) {
+        mainImgFixed4.classList.add("img-hidden");
+        mainImgFixed3.classList.remove("img-hidden");
     }
-    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`
-}
+    else if (!mainImgFixed3.classList.contains("img-hidden")) {
+        mainImgFixed3.classList.add("img-hidden");
+        mainImgFixed2.classList.remove("img-hidden");
+    }
+    else if (!mainImgFixed2.classList.contains("img-hidden")) {
+        mainImgFixed2.classList.add("img-hidden");
+        mainImgFixed1.classList.remove("img-hidden");
+    }
+    else if (!mainImgFixed1.classList.contains("img-hidden")) {
+        mainImgFixed1.classList.add("img-hidden");
+        mainImgFixed4.classList.remove("img-hidden");
+    }
+});
