ddf9be22d21897510e5b64e094c8e3a84351ee38
Miñia Rx115 <murciapradillarubenyair@gmail.com>
2023-11-19 18:45:03 -0500

    prueba para subir cambios

--- js/index.js
+++ js/index.js
@@ -57,22 +57,22 @@ function efectoHabilidades(){
     if(distancia_skills>=300 && entro==false){
         entro = true;
         const intervalHtml = setInterval(function(){
-            pintarBarra(html, 16, 0, intervalHtml);
+            pintarBarra(html, 9, 0, intervalHtml);
         },100);
         const intervalJavascript = setInterval(function(){
-            pintarBarra(javascript, 11, 1, intervalJavascript);
+            pintarBarra(javascript, 7, 1, intervalJavascript);
         },100);
         const intervalWordpress = setInterval(function(){
-            pintarBarra(wordpress, 11, 2, intervalWordpress);
+            pintarBarra(wordpress, 15, 2, intervalWordpress);
         },100);
         const intervalPhotoshop = setInterval(function(){
-            pintarBarra(photoshop, 15, 3, intervalPhotoshop);
+            pintarBarra(photoshop, 13, 3, intervalPhotoshop);
         },100);
         const intervalPhp = setInterval(function(){
             pintarBarra(php, 16, 4, intervalPhp);
         },100);
         const intervalIlustrator = setInterval(function(){
-            pintarBarra(ilustrator, 11, 5, intervalIlustrator);
+            pintarBarra(ilustrator, 15, 5, intervalIlustrator);
         },100);
     }
 }
